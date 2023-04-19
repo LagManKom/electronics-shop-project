9ce2a5f7c135cfbfd6e67cb1dcfc6d0f5a19d30b
LagManKom <117903818+LagManKom@users.noreply.github.com>
2023-04-19 15:54:59 +0400

    creating attributes and methods. Writing tests

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,20 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+"""Nесты с использованием pytest для модуля item."""
+import pytest
+
+from src.item import Item
+
+
+@pytest.fixture
+def item():
+    return Item('Xiaomi', 10000, 2)
+
+
+def test_item_calculate(item):
+    assert item.calculate_total_price() == 20000
+
+
+def test_apply_discount(item):
+    item.pay_rate = 0.50
+    item.apply_discount()
+
+    assert item.calculate_total_price() == 10000
