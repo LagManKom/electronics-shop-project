51cad900bd265c972160165cd806fdee418aa5d5
LagManKom <117903818+LagManKom@users.noreply.github.com>
2023-04-24 19:05:43 +0400

    homework 2

--- src/item.py
+++ src/item.py
@@ -1,3 +1,6 @@
+import csv
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,11 +16,14 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
 
         self.all.append(self)
+
+    def __repr__(self):
+        return self.__name
 
     def calculate_total_price(self) -> float:
         """
@@ -32,3 +38,25 @@ class Item:
         Применяет установленную скидку для конкретного товара.
         """
         self.price *= self.pay_rate
+
+    @property
+    def name(self) -> str:
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        if len(name) <= 10:
+            self.__name = name
+        else:
+            print('Exception: Длина наименования товара превышает 10 символов')
+
+    @classmethod
+    def instantiate_from_csv(cls, path='./src/items.csv'):
+        with open(path, 'r', newline='', encoding='windows-1251') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                cls(row['name'], row['price'], row['quantity'])
+
+    @staticmethod
+    def string_to_number(data):
+        return int(float(data))
--- tests/test_item.py
+++ tests/test_item.py
@@ -18,3 +18,21 @@ def test_apply_discount(item):
     item.apply_discount()
 
     assert item.calculate_total_price() == 10000
+
+
+def test_name(item):
+    assert item.name == 'Xiaomi'
+
+
+def test_name_setter(item):
+    item.name = 'Смартфон'
+    assert item.name == 'Смартфон'
+
+
+def test_instantiate_form_csv():
+    Item.instantiate_from_csv()
+    assert len(Item.all) == 9
+    assert Item.string_to_number('5') == 5
+    assert Item.string_to_number('5.0') == 5
+    assert Item.string_to_number('5.5') == 5
+
